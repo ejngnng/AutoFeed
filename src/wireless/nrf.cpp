4738a5fdb0c7d46c06bb9c9f74c6332839bac6dc
ninja <ningjiang@baicells.com>
2017-11-09 17:13:47 +0800

    add nrf24L01

--- src/wireless/nrf.cpp
+++ src/wireless/nrf.cpp
@@ -7,3 +7,53 @@
 */
 
 #include "nrf.h"
+
+RF24 radio(NRF_CE, NRF_CS);   // CE, CS Set up nRF24L01 radio on SPI bus plus pins 7 & 8
+
+const uint64_t pipes[2] = { 0xABCDABCD71LL, 0x544d52687CLL };   // Radio pipe addresses for the 2 nodes to communicate.
+
+void nrf_init(nrf_role_m role){
+  radio.begin();                           // Setup and configure rf radio
+  radio.setChannel(1);
+  radio.setPALevel(RF24_PA_MAX);
+  radio.setDataRate(RF24_1MBPS);
+  radio.setAutoAck(1);                     // Ensure autoACK is enabled
+  radio.setRetries(2,15);                  // Optionally, increase the delay between retries & # of retries
+
+  radio.setCRCLength(RF24_CRC_8);          // Use 8-bit CRC for performance
+
+  radio.openWritingPipe(pipes[0]);
+  radio.openReadingPipe(1,pipes[1]);
+
+  radio.startListening();                 // Start listening
+  radio.printDetails();                   // Dump the configuration of the rf unit for debugging
+
+  radio.powerUp();                        //Power up the radio
+
+  if(role == RF_TX){
+    radio.openWritingPipe(pipes[1]);
+    radio.openReadingPipe(1,pipes[0]);
+    radio.stopListening();
+  }else{
+    radio.openWritingPipe(pipes[0]);
+    radio.openReadingPipe(1,pipes[1]);
+    radio.startListening();
+  }
+
+}
+
+
+nrf_rs_t set_ctl(ctl_obj_t *ctl_obj){
+  if(!radio.writeFast(ctl_obj, sizeof(ctl_obj_t))){
+    return RS_FAIL;
+  }
+  return RS_OK;
+}
+
+
+void get_ctl(ctl_obj_t *ctl_obj){
+  memset(ctl_obj, 0, sizeof(ctl_obj_t));
+  while(radio.available()){
+    radio.read(ctl_obj, sizeof(ctl_obj_t));
+  }
+}
--- src/wireless/nrf.h
+++ src/wireless/nrf.h
@@ -11,9 +11,46 @@
 
 #include <Arduino.h>
 
+#include <SPI.h>
+#include "RF24.h"
+
+#define NRF_CE    7
+#define NRF_CS    8
+
 typedef enum nrf_rs_e{
   RS_OK = 0,
   RS_FAIL
 }nrf_rs_t;
+
+typedef enum nrf_role_e{
+  RF_RX = 0,
+  RF_TX
+}nrf_role_m;
+
+typedef struct control_obj_s{
+  unsigned char left_ctl;
+  unsigned char right_ctl;
+  unsigned char up_ctl;
+  unsigned char down_ctl;
+  unsigned char stop_ctl;
+  unsigned char start_ctl;
+  unsigned char shoot_ctl;
+}ctl_obj_t;
+
+typedef enum control_type_e{
+  STOP = 0,
+  START,
+  LEFT,
+  RIGHT,
+  UP,
+  DOWN,
+  SHOOT
+}ctl_type_m;
+
+void nrf_init(nrf_role_m role);
+
+nrf_rs_t set_ctl(ctl_obj_t *ctl_obj);
+
+void get_ctl(ctl_obj_t *ctl_obj);
 
 #endif
